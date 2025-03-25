6727a719fd671da426301a8520af29329810f430
wenawo153 <153762815+wenawo153@users.noreply.github.com>
2025-03-25 11:59:24 +0300

    w

--- src/AltSKUF.Back.Authentication/obj/Debug/net9.0/AltSKUF.Back.Authentication.AssemblyInfo.cs
+++ src/AltSKUF.Back.Authentication/obj/Debug/net9.0/AltSKUF.Back.Authentication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AltSKUF.Back.Authentication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c00f821e1c3fc038616a121ec7decb1d21a5ad24")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+360bf3b37dba75c61650782a339c272978004f41")]
 [assembly: System.Reflection.AssemblyProductAttribute("AltSKUF.Back.Authentication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AltSKUF.Back.Authentication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
